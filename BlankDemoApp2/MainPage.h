829db5c8108255589fea67b578fd3f33846c997b
Rajendrasing Pardeshi <rpardesh@microsoft.com>
2021-12-05 17:23:12 +0530

    Initial change

---
+++ BlankDemoApp2/MainPage.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::BlankDemoApp2::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::BlankDemoApp2::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
